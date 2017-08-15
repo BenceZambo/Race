2823d08d28528fe5b55e7f39acc423d80211d2a2
BenceZambo <bence.zambo96@gmail.com>
2017-08-15 23:59:53 +0200

    in main createVehicles method created

--- src/com/car_race/CarRace.java
+++ src/com/car_race/CarRace.java
@@ -1,7 +1,30 @@
 package com.car_race;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRace {
+    static List<car> cars = new ArrayList<car>();
+    static List<truck> trucks = new ArrayList<truck>();
+    static List<motorcycle> motorcycles = new ArrayList<motorcycle>();
+
+
     public static void main(String[] args) {
 
+        createVehicles();
+    }
+
+    public static void createVehicles(){
+        for (int i=1; i<11; i++){
+            cars.add(new car());
+        }
+
+        for (int i=1; i<11; i++){
+            trucks.add(new truck());
+        }
+
+        for (int i=1; i<11; i++){
+            motorcycles.add(new motorcycle());
+        }
     }
 }
