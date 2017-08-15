9f57e7395c59174a1db7e843d9d02b01d0be6cd5
BenceZambo <bence.zambo96@gmail.com>
2017-08-15 16:53:06 +0200

    vehicle, and car classes filled with variables and methods

--- src/com/car_race/car.java
+++ src/com/car_race/car.java
@@ -1,4 +1,43 @@
 package com.car_race;
 
+import java.util.Random;
+
 public class car extends vehicles {
+    String[] names = {"Meridium", "Zeal", "Realm", "Paradox", "Vigor",
+                        "Thunder", "Primade", "Inception", "Charm", "Portrait"};
+    Boolean isSpeedLimited = false;
+
+    Random random = new Random();
+
+    public void setname(){
+        name = names[random.nextInt(10)] + names[random.nextInt(10)];
+    }
+
+    public void setSpeedLimit(int limit){
+        if (limit <= 3){
+            speed = 70;
+        }
+    }
+
+    public void setSpeed(){
+        int randomSpeed = random.nextInt(4);
+        if (randomSpeed == 0 && isSpeedLimited){
+            speed = 80;
+        }
+        else if (randomSpeed == 1 && isSpeedLimited){
+            speed = 90;
+        }
+        else if (randomSpeed == 2 && isSpeedLimited){
+            speed = 100;
+        }
+        else if (randomSpeed == 3 && isSpeedLimited){
+            speed = 110;
+        }
+    }
+
+    public void moveForAnHour(){
+        setSpeed();
+        distanceTraveled += speed;
+    }
+
 }
--- src/com/car_race/vehicles.java
+++ src/com/car_race/vehicles.java
@@ -1,4 +1,7 @@
 package com.car_race;
 
 public class vehicles {
+    int distanceTraveled;
+    int speed;
+    String name;
 }
